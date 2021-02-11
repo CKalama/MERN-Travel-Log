c3e818bc6225c6d26932cc0a1f0e1251d529ae3b
Chris Kalama <chriskalama96@gmail.com>
2021-02-11 00:49:57 -0600

    psuedocoded and wrote a middleware route that will help with debugging and errors

--- server/src/index.js
+++ server/src/index.js
@@ -23,6 +23,26 @@ app.get( "/", (req, res) => {
     });
 });
 
+//This Middleware was built with a very Tecnical Background, essentially, when you test for these errors and you jump to a /route that is not found, you will get a error message displaying why this is happening and where it may be in the code a little better. Super useful for debugging!!!
+
+//With this Middleware, which requires THREE PARAMETERS, This is usually the last route you create which is the notFound route. It is a debugging error catching route, originalUrl is a good syntax to remember as well
+//This middleware is creating the Not Found Error, sending the 404 status code so we know its 404, and then forwarding to actual error handler.
+app.use((req, res, next) => {
+    const err = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404)
+    next(err)
+});
+
+//Error-Handling Middlewares require FOUR PARAMETERS . This will help us check if statusCode is 200 and that means some other route has an error and we made it to this error. It has a ternary operator
+app.use((error, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+    res.status(statusCode);
+    res.json({
+        message: error.message,
+        stack: error.stack,
+    })
+})
+
 
 
 app.listen(PORT, () => {
