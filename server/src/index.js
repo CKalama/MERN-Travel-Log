af27cef92b5e7506d5f583f02943c92ca161223f
Chris Kalama <chriskalama96@gmail.com>
2021-02-11 00:17:26 -0600

    added simple app.get to test if we can get info to display

--- server/src/index.js
+++ server/src/index.js
@@ -4,13 +4,26 @@ const express = require("express")
 const morgan = require("morgan"); 
 //Within the Network on Inspect, within the Headers when you get a localhost error, you can see what routing system you use (i.e Express), Helmet encrypts this and helps with hackers. 
 const helmet = require("helmet")
+// cross-origin-resource-sharing header. Sets that to * meaning any origin can request from our backend. For ours, it includes localhost:3000 b/c that is what React runs on. With cors, we are able to say 'hey if you see this localhost:3000 pop up on the frontend then you can communicate with our backend' 
+const cors = require("cors")
 
 const app = express();
-app.use(morgan('common'));
-app.use(helmet());
-
-
 const PORT = process.env.PORT || 1337
+//Middlewares running through Express Instance... 
+app.use(morgan('combined'));
+app.use(helmet());
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+
+
+app.get( "/", (req, res) => {
+    res.json({
+        message:"Hello World!",
+    });
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`SERVER IS FIRED UP AT http://localhost:${PORT}`)
