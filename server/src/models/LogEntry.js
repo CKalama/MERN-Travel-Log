5f1b41e831af552d642d85fc4892102de13f6125
Chris Kalama <chriskalama96@gmail.com>
2021-02-11 20:04:30 -0600

    added a basic models folder with a LogEntry schema with model sample Data, still looking at mongoose library to figure out more specific types

---
+++ server/src/models/LogEntry.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose; 
+//If we put this into an object we can validate it before it is inserted into database. And then we can put into a variable so that we can reuse it!
+const requiredString = {
+    type: String,
+    required: true,
+}
+const requiredNumber = {
+    type: Number,
+    required: true,
+}
+
+const logEntrySchema = new Schema({
+    title: requiredString,
+    description: String,
+    comments: String,
+    image: String, 
+    rating: {
+        type: Number,
+        min:[0, `C'mon don't ruin their day!`],
+        max:[10, `It does look great!`],
+        default:0,
+    },
+    latitude: requiredNumber,
+    longitude: requiredNumber,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        
+    }
+})
