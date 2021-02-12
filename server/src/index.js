27196ee3bcfd33b554fd66b5d5328768be9d7a5a
Chris Kalama <chriskalama96@gmail.com>
2021-02-11 20:59:09 -0600

    started mongoose connection code in index.js, database is TRAVEL_LOG_DATABASE_URL for now but might need to be changed to DATABASE_URL

--- server/src/index.js
+++ server/src/index.js
@@ -7,10 +7,19 @@ const morgan = require("morgan");
 const helmet = require("helmet")
 // cross-origin-resource-sharing header. Sets that to * meaning any origin can request from our backend. For ours, it includes localhost:3000 b/c that is what React runs on. With cors, we are able to say 'hey if you see this localhost:3000 pop up on the frontend then you can communicate with our backend' 
 const cors = require("cors");
+
+const mongoose = require("mongoose");
+
 //importing middleware consts
 const { notFound, errorHandler } = require("./middlewares");
 
 const app = express();
+
+//Creating Mongoose connection. You can set up the mongoose library works is we can create a connection here or elsewhere and because we registered the logEntry model we will be good to go. WE shoould use enviornment variables here. Such as process.env
+mongoose.connect(process.env.TRAVEL_LOG_DATABASE_URL, {
+    useNewUrlParser: true 
+});
+
 const PORT = process.env.PORT || 1337
 
 //Middlewares running through Express Instance... 
