97742dbd505ebacb3b01302b031ee7edd835967e
Chris Kalama <chriskalama96@gmail.com>
2021-02-16 02:01:34 -0600

    testing some post routes on postman and modified some env variables to get rid of deprecation errors

--- server/src/index.js
+++ server/src/index.js
@@ -22,7 +22,8 @@ const app = express();
 
 //Creating Mongoose connection. You can set up the mongoose library works is we can create a connection here or elsewhere and because we registered the logEntry model we will be good to go. WE shoould use enviornment variables here. Such as process.env
 mongoose.connect("mongodb://localhost/travel-log", {
-    useNewUrlParser: true 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
 const PORT = process.env.PORT || 1337
