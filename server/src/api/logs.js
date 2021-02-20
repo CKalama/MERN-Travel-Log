feba056633e16cc0089d081f23a88c332c3c5166
Chris Kalama <chriskalama96@gmail.com>
2021-02-19 18:36:31 -0600

    figured out Postman issue and needed to download desktop agent to get around CORS issue, after figuring out, wrote a try catch js function to try and get POST route to fire off.

--- server/src/api/logs.js
+++ server/src/api/logs.js
@@ -17,10 +17,24 @@ router.get('/', (req,res) => {
 
 //Creating post route, the handler that runs when we receive a post request from the /logs route
         //jsonParser needs to be inbetween 2 parameters if you get body parser middleware back //
-router.post('/', (req, res) => {
-    //Going back to our LogEntry.js in the models folder, we want to create a new one with this variable
-    const logEntry = new LogEntry(req.body);
-    console.log(req.body);
+        
+//Going to use an async parameter here how mongoose documentation wants us to (https://mongoosejs.com/docs/models.html#constructing-documents)
+//Need next so that the catch (error) will grab our middleware error we created
+router.post('/', async (req, res, next) => {
+    //Need to wrap this in a try / catch javascript function
+    try {
+        //Going back to our LogEntry.js in the models folder, we want to create a new one with this variable
+        const logEntry = new LogEntry(req.body);
+        const createdEntry = await logEntry.save();
+        res.json(createdEntry);
+    } 
+    catch (error) {
+        //need the Next from the parameter above so it hits our error in middleware.js
+        next(error);
+    }
+
+    
+    
 })
 
 
