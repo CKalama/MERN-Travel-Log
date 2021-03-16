460b0dc45a0812123fc0d04d85fecbb782ee1f49
Chris Kalama <chriskalama96@gmail.com>
2021-03-15 23:57:09 -0500

    added 4 parameters from react-hook-form to LogEntry component. Following the SDK information, registered the form and console logged the data, now the form data gets pushed to the console. Also, due to JSX changed every 'for' to 'htmlFor'

--- client/src/Components/LogEntryForm.js
+++ client/src/Components/LogEntryForm.js
@@ -2,26 +2,36 @@ import React from "react";
 import { useForm } from "react-hook-form"; 
 
 const LogEntryForm = () => {
+    //These are special to react-hook-form. 
+    //register will need to be registered to our entire form, need to do on inputs and textareas, it will take the name and create an object of all the data. 
+    const { register, handleSubmit, watch, errors } = useForm();
+
+    //Gives us access to the data on the form. This will also be the data we are sending to our backend server!
+    const formSubmit = (data) => {
+        console.log(data);
+    }
+
     return (
-        <form className="entry-form">
-            <label for="title">Title</label>
-            <input name="title" required/>
+        <form onSubmit={handleSubmit(formSubmit)} className="entry-form">
+
+            <label htmlFor="title">Title</label>
+            <input name="title" required ref={register}/>
             <br />
             
-            <label for="comments">Comments</label>
-            <textarea name="comments" rows={3}/>
+            <label htmlFor="comments">Comments</label>
+            <textarea name="comments" rows={3} ref={register}/>
             <br />
 
-            <label for="description">Description</label>
-            <textarea name="description" rows={3}/>
+            <label htmlFor="description">Description</label>
+            <textarea name="description" rows={3} ref={register}/>
             <br />
 
-            <label for="image">Image</label>
-            <input name="image" />
+            <label htmlFor="image">Image</label>
+            <input name="image" ref={register}/>
             <br />
 
-            <label for="visitDate">Visited Date</label>
-            <input name="visitDate" type="date" />
+            <label htmlFor="visitDate">Visited Date</label>
+            <input name="visitDate" type="date" required ref={register} />
             <br />
 
             <button>Submit Your Travel Location!</button>
