d39eb8ad86e69d1f3ff359ae606f86d26641dd70
Chris Kalama <chriskalama96@gmail.com>
2021-03-16 00:44:32 -0500

    on LogEntry component, added two new UseState hooks, these will help so we don't need to refresh the page for a new pin to appear and also to throw an error if the database is down or for whatever reason the client can't reach the server

--- client/src/Components/LogEntryForm.js
+++ client/src/Components/LogEntryForm.js
@@ -1,9 +1,12 @@
 import React from "react";
+import {useState} from "react"
 import { useForm } from "react-hook-form"; 
 import {createLogEntry} from "../API";
 
 //We added this location prop because on App.js we set a requirement that long and lat are needed for the form to be validated. Instead of doing that here, we passed it through a prop that is already doing the work for us in map-gl which will help a lot!
 const LogEntryForm = ( { location } ) => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('')
     //These are special to react-hook-form. 
     //register will need to be registered to our entire form, need to do on inputs and textareas, it will take the name and create an object of all the data. 
     const { register, handleSubmit, watch, errors } = useForm();
@@ -11,12 +14,14 @@ const LogEntryForm = ( { location } ) => {
     //Gives us access to the data on the form. This will also be the data we are sending to our backend server!
     const formSubmit = async (data) => {
         try {
+            setLoading(true);
             data.latitude = location.latitude;
             data.longitude = location.longitude;
             const createdEntry = await createLogEntry(data)
             console.log(createdEntry)
         } catch (error) {
             console.error(error)
+            setError(error.message);
         }
     }
 
