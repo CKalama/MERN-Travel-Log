72e60955835d3cb030306fe941917b852391d789
Chris Kalama <chriskalama96@gmail.com>
2021-03-24 00:04:57 -0500

    reverting back to old API url

--- client/src/API.js
+++ client/src/API.js
@@ -2,18 +2,18 @@
 //We will be using fetch and async functions. 
 
 const API_URL = "http://localhost:8080";
-const API_URL2 = "https://secure-falls-24524.herokuapp.com/";
+//const API_URL2 = "https://secure-falls-24524.herokuapp.com/";
 
 
 //we can export this function by simply putting export at the beginning of the function. async allows us to call this function at any time
 export async function listLogEntries() {
-    const response = await fetch(`${API_URL2}/api/logs`);
+    const response = await fetch(`${API_URL}/api/logs`);
     return response.json(); 
 }
 
 //This will be to create a LogEntry on our LogEntry Component. We will need to make a POST request.
 export async function createLogEntry(entry) {
-    const response = await fetch (`${API_URL2}/api/logs`, {  
+    const response = await fetch (`${API_URL}/api/logs`, {  
         method: 'POST', 
         headers: {
             'content-type': 'application/json',
